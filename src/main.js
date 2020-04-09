dc4805ba7c44d6acf376ac71c9d79db3bbf820f0
Сергей Дербенцев <modcom@rambler.ru>
2020-04-09 11:08:59 +0300

    Отделил данные от представления, создал для компонентов структуру данных

--- src/main.js
+++ src/main.js
@@ -1,28 +1,47 @@
-const TASK_COUNT = 3;
-
 import {createSiteMenuTemplate} from './components/menu.js';
 import {createFilterTemplate} from './components/filter.js';
 import {createSortTemplate} from './components/sort.js';
-import {createFormTemplate} from './components/form.js';
-import {createCardTemplate} from './components/card.js';
+import {createTaskTemplate} from './components/task.js';
 import {createLoadButtonTemplate} from './components/loadButton.js';
+import {generateFilters} from './mocks/filter.js';
+import {generateTasks} from './mocks/task.js';
 import {render} from './components/rendering.js';
+import {createTaskEditTemplate} from './components/taskEdit.js';
+
+const TASK_COUNT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
+const filters = generateFilters();
+const tasks = generateTasks(TASK_COUNT);
+
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
-render(siteMainElement, createFilterTemplate(), `beforeend`);
+render(siteMainElement, createFilterTemplate(filters), `beforeend`);
 render(siteMainElement, createSortTemplate(), `beforeend`);
 
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 const boardElement = siteMainElement.querySelector(`.board`);
 
-render(taskListElement, createFormTemplate(), `beforeend`);
+render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createCardTemplate(), `beforeend`);
+for (let i = 1; i < SHOWING_TASKS_COUNT_ON_START; i++) {
+  render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
 render(boardElement, createLoadButtonTemplate(), `beforeend`);
+
+const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+  tasks.slice(prevTasksCount, showingTasksCount)
+    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+});
