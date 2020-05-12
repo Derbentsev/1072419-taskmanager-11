8ba9f8a970a61226741932f99b83a963f649601e
Сергей Дербенцев <modcom@rambler.ru>
2020-05-12 10:03:29 +0300

    Отрефакторил main.js

--- src/main.js
+++ src/main.js
@@ -29,25 +29,6 @@ import {
 } from './models/tasks';
 
 
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-const siteMenuComponent = new SiteMenu();
-
-render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
-
-const tasks = generateTasks(TASK_COUNT);
-const tasksModel = new Tasks();
-tasksModel.setTasks(tasks);
-
-const filterController = new FilterController(siteMainElement, tasksModel);
-filterController.render();
-
-const boardComponent = new Board();
-render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
-
-const boardController = new BoardController(boardComponent, tasksModel);
-boardController.render();
-
 const dateTo = new Date();
 const dateFrom = (() => {
   const d = new Date(dateTo);
@@ -55,7 +36,23 @@ const dateFrom = (() => {
   return d;
 })();
 
+const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new Tasks();
+tasksModel.setTasks(tasks);
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenu();
 const statisticsComponent = new Statistics({tasks: tasksModel, dateFrom, dateTo});
+const boardComponent = new Board();
+const boardController = new BoardController(boardComponent, tasksModel);
+const filterController = new FilterController(siteMainElement, tasksModel);
+
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+filterController.render();
+render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
+boardController.render();
+
 render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
 statisticsComponent.hide();
 
